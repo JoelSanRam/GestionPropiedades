4377cec27ac981e984692160bf3abc6fd7b1b425
alexisCode50 <55004811+alexisCode50@users.noreply.github.com>
2021-01-11 17:10:01 -0600

    modificamos boton para crear usuarios

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -8,7 +8,7 @@
     </div>
     <div class="col-lg-2">
         @if (Auth::user()->rol == "Administrador")
-            <a href="{{ route('usuarios.index') }}" class="btn text-light" style="background: #ffc800;">Nuevo Usuario</a>
+            <a href="{{ route('usuarios.create') }}" class="btn text-light" style="background: #ffc800;">Nuevo Usuario</a>
         @endif
     </div>
 </div>
