d3e0e419c48dd19ee527092524430907f98f827e
alexisCode50 <55004811+alexisCode50@users.noreply.github.com>
2021-01-10 22:55:24 -0600

    bug al insertar datos con excel ready, falta validaciones para el excel

--- app/Imports/InformacionImport.php
+++ app/Imports/InformacionImport.php
@@ -8,62 +8,57 @@ use App\Dimencion;
 use App\Valor;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Imports\HeadingRowFormatter;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-HeadingRowFormatter::default('none');
-
-class InformacionImport implements ToCollection
+class InformacionImport implements ToCollection, WithHeadingRow
 {
 
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) 
         {
-            /*User::create([
-                'name' => $row[0],
-            ]);*/
 
             Propiedad::create([
-                'origen_id' => $row[0],
-                'tipo' => $row[1],
-                'granja' => $row[2],
-                'estatus' => $row[3],
-                'nombre_corto' => $row[4],
-                'observaciones' => $row[5],
-                'propietario' => $row[6],
-                'entidad_federativa' => $row[7],
-                'municipio' => $row[8],
-                'localidad' => $row[9],
-                'folio_regpub' => $row[10],
-                'folio_catastral' => $row[11],
+                'origen_id' => $row['origen_id'],
+                'tipo' => $row['tipo'],
+                'granja' => $row['granja'],
+                'estatus' => $row['estatus'],
+                'nombre_corto' => $row['nombre_corto'],
+                'observaciones' => $row['observaciones'],
+                'propietario' => $row['propietario'],
+                'entidad_federativa' => $row['entidad_federativa'],
+                'municipio' => $row['municipio'],
+                'localidad' => $row['localidad'],
+                'folio_regpub' => $row['folio_regpub'],
+                'folio_catastral' => $row['folio_catastral'],
             ]);
 
             Ubicacion::create([
-                'ejido' => $row[12],
-                'parcela' => $row[13],
-                'lote' => $row[14],
-                'solar' => $row[15],
-                'tablaje' => $row[16],
-                'finca' => $row[17],
-                'direccion' => $row[18],
-                'ejido_manzana' => $row[19],
-                'codigo_postal' => $row[20],
+                'ejido' => $row['ejido'],
+                'parcela' => $row['ejido_parcela'],
+                'lote' => $row['ejido_lote'],
+                'solar' => $row['solar'],
+                'tablaje' => $row['tablaje'],
+                'finca' => $row['finca'],
+                'direccion' => $row['direccion'],
+                'ejido_manzana' => $row['ejido_manzana'],
+                'codigo_postal' => $row['codigo_postal'],
             ]);
 
             Dimencion::create([
-                'superficie_construccion' => $row[21],
-                'superficie_terreno' => $row[22],
-                'frente' => $row[23],
-                'fondo' => $row[24],
-                'capacidad_granja' => $row[25],
+                'superficie_construccion' => $row['superficie_construccion'],
+                'superficie_terreno' => $row['superficie_terreno'],
+                'frente' => $row['frente'],
+                'fondo' => $row['fondo'],
+                'capacidad_granja' => $row['capacidad_granja'],
             ]);
 
             Valor::create([
-                'valor_construccion' => $row[26],
-                'valor_comercial' => $row[27],
-                'fecha_valor_comercial' => $row[28],
-                'valor_catastral' => $row[29],
-                'fecha_valor_catastral' => $row[30],
+                'valor_construccion' => $row['valor_construccion'],
+                'valor_comercial' => $row['valor_comercial'],
+                'fecha_valor_comercial' => $row['fecha_valor_comercial'],
+                'valor_catastral' => $row['valor_catastral'],
+                'fecha_valor_catastral' => $row['fecha_valor_catastral'],
             ]);
 
         }
